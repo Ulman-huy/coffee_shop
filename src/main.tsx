5ea9c92550a0a1f5151e817175735e3bda337803
Ulman-huy <buihuy.lh@gmail.com>
2023-11-20 16:17:23 +0700

    deploy firebase

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,25 @@ import { persistor, store } from "./redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6q5Q3R7d1iB2UMR1p6tiSMrqwIO367pc",
+  authDomain: "coffee-shop-58f1e.firebaseapp.com",
+  projectId: "coffee-shop-58f1e",
+  storageBucket: "coffee-shop-58f1e.appspot.com",
+  messagingSenderId: "931609471296",
+  appId: "1:931609471296:web:6056ddeea689fa8e1732a5"
+};
+
+// Initialize Firebase
+// @ts-nocheck
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
